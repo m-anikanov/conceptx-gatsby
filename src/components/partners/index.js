ed14406286edc5caa676fc37b440505d7ed07760
manikanov <misha.anikanov@gmail.com>
2024-12-25 21:08:28 +0100

    elmos link

--- src/components/partners/index.js
+++ src/components/partners/index.js
@@ -21,10 +21,29 @@ export const Partners = () => {
             width={100}
           />
           <section className={styles.itemData}>
-            <section className={styles.itemTitle}>LiveDrive</section>
+            <section className={styles.itemTitle}>LiveDrive.rent</section>
             <section className={styles.itemDescription}>
               Доступная и надёжная аренда автомобилей
               <br />в Тбилиси и по всей Грузии.
+            </section>
+          </section>
+        </a>
+        <a
+          href="https://elmos.tech"
+          className={styles.item}
+          target="__blank"
+          id="partners"
+        >
+          <img
+            className={styles.partnerImg}
+            src="/elmos-logo.webp"
+            alt="Логотип партнера Elmos.tech"
+            width={100}
+          />
+          <section className={styles.itemData}>
+            <section className={styles.itemTitle}>Elmos.tech</section>
+            <section className={styles.itemDescription}>
+            Пневматическое оборудование, а также разработка и производство систем автоматизации.
             </section>
           </section>
         </a>
